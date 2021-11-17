0f0c0af1b9a83d6aae0713af71468dc84b4c715b
Mohammad Faizan Raza <faizanrazaab@gmail.com>
2021-11-17 13:47:11 +0530

    code of the project

---
+++ project.py
@@ -0,0 +1,20 @@
+import random
+
+print("Hello!! Welcome to the passward generator")
+input(print("press enter to get start"))
+passlen = int(input("How many digits of passward do you want to create"))
+
+a = input("do you want to add special charactor. Enter Y/N")
+if a == Y or a == y:
+  charno = int(input("How many special charactors do you want to add"))
+elif a == N or a == n:
+  charno == 0
+else :
+  print("you have entered a wrong key")
+
+passward = ""
+for(i = 0 , i < passlen-1 , i++):
+  random.rand
+
+
+
